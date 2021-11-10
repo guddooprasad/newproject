d2de9604505667e9d7178aaa77a7de4b621301e0
guddooprasad <guddooprasad001122@gmail.com>
2021-11-10 14:21:43 +0530

    second commit

--- src/screen/HomeScreen/index.js
+++ src/screen/HomeScreen/index.js
@@ -1,21 +1,37 @@
-import react from 'react'
+import React from 'react'
 import Input from '../../components/Input'
 
 const HomeScreen = () =>{
 
- const onInput = (value) =>{
-     // const url = `https://s.99acres.com/api/autocomplete/suggest?term=${value}&PREFERENCE=R&RESCOM=R&FORMAT=APP`
+ const onInput = async (value) =>{
+      const url = 'http://99acres.com/api-aggregator/static-attributes?includedFields='
+      //const url = `https://s.99acres.com/api/autocomplete/suggest?term=${value}&PREFERENCE=R&RESCOM=R&FORMAT=APP`
     
-     const url = 'https://jsonplaceholder.typicode.com/todos/1'
-      fetch(url)
-     .then(response => {
-        // handle the response
-        console.log(response)
-      })
-     .catch(error => {
-        // handle the error
-        console.log(error)
-    });
+     //const url = 'https://jsonplaceholder.typicode.com/todos/1'
+      // await fetch(url)
+        
+        
+        
+        
+    //  .then(response => {
+    //    //console.log('guddoo')
+    //     // handle the response
+    //     const data = response.json();
+    //     console.log(data)
+    //   })
+    //  .catch(error => {
+    //     // handle the error
+    //     console.log(error)
+    // });
+   
+    
+    const response = await fetch("http://www.99acres.com/api-aggregator/static-attributes?includedFields=");
+    const data = await response.json();
+    
+   
+    console.log(data.cities);
+    
+    
 
    }
 
